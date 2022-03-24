816d66c6265db873f97254cbf00063d27f4b8cfd
raphaelbenevides18 <raphael.rbinformatica@gmail.com>
2022-03-24 12:11:15 -0300

    primeiro commit do projeto

---
+++ src/main/java/br/com/rlb/projetoOriginal/ProjetoOriginalApplication.java
@@ -0,0 +1,13 @@
+package br.com.rlb.projetoOriginal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOriginalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOriginalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rlb/projetoOriginal/ProjetoOriginalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rlb.projetoOriginal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoOriginalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
